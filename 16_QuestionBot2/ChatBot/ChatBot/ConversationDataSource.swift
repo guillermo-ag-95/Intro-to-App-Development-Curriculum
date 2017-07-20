a2ef6cdf57fd65a6447e935d4f6fd5260308bb62
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-07-20 12:49:01 +0200

    Lesson 16: QuestionBot 2 completed

--- 16_QuestionBot2/ChatBot/ChatBot/ConversationDataSource.swift
+++ 16_QuestionBot2/ChatBot/ChatBot/ConversationDataSource.swift
@@ -2,21 +2,36 @@ import Foundation
 class ConversationDataSource {
     
     /// The number of Messages in the conversation
-    let messageCount = 0
+    var messageCount: Int {
+        return messages.count
+    }
+    
+    /// The ordered list of questions and answers
+    var messages: [Message] = [openingLine]
+
     
     /// Add a new question to the conversation
     func add(question: String) {
-        print("Asked to add question: \(question)")
+        
+        let message = Message(date: Date(), text: question, type: .question)
+        messages.append(message)
+        
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
-        print("Asked to add answer: \(answer)")
+        
+        let message = Message(date: Date(), text: answer, type: .answer)
+        messages.append(message)
+        
     }
     
     /// The Message at a specific point in the conversation
     func messageAt(index: Int) -> Message {
+        
         print("Asking for message at index \(index)")
-        return Message(date: Date(), text: "Hello, world!", type: .answer)
+        return messages[index]
+        
     }
+    
 }
