b443958011db58fd1ba712a0e68f250605c5f4e4
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-07-20 18:50:27 +0200

    Lesson 17: Actions and Outlets completed

--- 17_ActionsAndOutlets/ColorMix/ColorMix/ViewController.swift
+++ 17_ActionsAndOutlets/ColorMix/ColorMix/ViewController.swift
@@ -1,23 +1,32 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by Guillermo Alcalá Gamero on 20/7/17.
+//  Copyright © 2017 Guillermo Alcalá Gamero. All rights reserved.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         colorView.layer.borderWidth = 5
         colorView.layer.cornerRadius = 20
         colorView.layer.borderColor = UIColor.black.cgColor
         
-        updateControls()
         updateColor()
+        updateControls()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBOutlet weak var colorView: UIView!
+    
     @IBOutlet weak var redSwitch: UISwitch!
     @IBOutlet weak var greenSwitch: UISwitch!
     @IBOutlet weak var blueSwitch: UISwitch!
@@ -26,51 +35,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
+    
     @IBAction func switchChanged(_ sender: UISwitch) {
-        updateControls()
         updateColor()
+        updateControls()
     }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
         updateColor()
+        updateControls()
     }
     
-    func updateControls() {
-        redSlider.isEnabled = redSwitch.isOn
-        blueSlider.isEnabled = blueSwitch.isOn
-        greenSlider.isEnabled = greenSwitch.isOn
+    @IBAction func reset(_ sender: UIButton) {
+        
+        redSlider.value = 1
+        greenSlider.value = 1
+        blueSlider.value = 1
+        
+        redSwitch.setOn(false, animated: false)
+        greenSwitch.setOn(false, animated: false)
+        blueSwitch.setOn(false, animated: false)
+        
+        updateColor()
+        updateControls()
+        
     }
     
     func updateColor() {
+        
         var red: CGFloat = 0
         var green: CGFloat = 0
         var blue: CGFloat = 0
+        
         if redSwitch.isOn {
             red = CGFloat(redSlider.value)
         }
+        
         if greenSwitch.isOn {
             green = CGFloat(greenSlider.value)
         }
+        
         if blueSwitch.isOn {
             blue = CGFloat(blueSlider.value)
         }
         
         let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
         colorView.backgroundColor = color
     }
-    @IBAction func reset(_ sender: AnyObject) {
+    
+    func updateControls() {
         
-        redSlider.value = 1
-        blueSlider.value = 1
-        greenSlider.value = 1
+        redSlider.isEnabled = redSwitch.isOn
+        greenSlider.isEnabled = greenSwitch.isOn
+        blueSlider.isEnabled = blueSwitch.isOn
         
-        redSwitch.isOn = false
-        blueSwitch.isOn = false
-        greenSwitch.isOn = false
-        
-        updateControls()
-        updateColor()
     }
-
+    
 }
 
